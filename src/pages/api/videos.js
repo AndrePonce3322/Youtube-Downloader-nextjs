eb5a205b205633f74df0cb879c539b515c276d25
Andre <89555120+AndrePonce3322@users.noreply.github.com>
2024-01-26 22:59:14 -0600

    Adding console log to the API

--- src/pages/api/videos.js
+++ src/pages/api/videos.js
@@ -1,6 +1,9 @@
+let count = 0;
 export default async function handler(req, res) {
   const { maxResults } = req.query;
   const results = maxResults ? maxResults : 21;
+
+  console.log('Fetch VIDEOS count: ', ++count);
 
   const allVideos = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=${results}&regionCode=ES&key=${process.env.YOUTUBE_API_KEY}`;
   const getVideos = await fetch(allVideos);
