9b99b45b78445821bd180118e66cfcf7b2061114
Andre <89555120+AndrePonce3322@users.noreply.github.com>
2024-02-04 01:06:57 -0600

    Add favicon and Open Graph meta tags

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -13,10 +13,22 @@ export default function Layout({
     <>
       <Head>
         <title>{title || 'Youtube Downloader'}</title>
+
+        <link rel='icon' href='/icon.png' />
+
         <meta
           name='description'
           content='Descarga tus videos favoritos con Youtube Downloader, fácil, rápido y accesible. creada por andrepg.vercel.app'
         />
+        <meta property='og:image' content='icon.png' />
+        <meta property='og:image:height' content='630' />
+        <meta property='og:image:width' content='1200' />
+        <meta property='og:title' content='Youtube Downloader' />
+        <meta
+          property='og:description'
+          content='Descarga tus videos favoritos con Youtube Downloader, fácil, rápido y accesible. creada por andrepg.vercel.app'
+        />
+        <meta property='og:url' content='https://youtube-hn.vercel.app/' />
       </Head>
       <main className={className}>{children}</main>
     </>
