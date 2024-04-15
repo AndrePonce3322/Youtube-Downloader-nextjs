0b902858b583fcc0aa195531845a21708945ab09
Andre <89555120+AndrePonce3322@users.noreply.github.com>
2024-04-15 03:43:36 -0600

    Actualizar enlaces de favicon y descripción en layout.tsx

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -14,16 +14,16 @@ export default function Layout({
       <Head>
         <title>{title || 'Youtube Downloader'}</title>
 
-        <link rel='icon' href='/favicon.png' />
+        <link rel='icon' href='/icon.png' />
 
         <meta
           name='description'
-          content='Descarga tus videos favoritos con Youtube Downloader, fácil, rápido y accesible. creada por andrepg.vercel.app'
+          content='Descarga tus videos favoritos con Youtube Downloader, fácil, rápido y accesible. Desarrollado por andrepg.me'
         />
         <meta property='og:title' content='Youtube Downloader' />
         <meta
           property='og:description'
-          content='Descarga tus videos favoritos con Youtube Downloader, fácil, rápido y accesible. creada por andrepg.vercel.app'
+          content='Descarga tus videos favoritos con Youtube Downloader, fácil, rápido y accesible. creada por andrepg.me'
         />
         <meta property='og:url' content='https://youtube-hn.vercel.app/' />
       </Head>
